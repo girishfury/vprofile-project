2f4e81faa8790bed0d27fedb5bde3056825db6e4
girishfury <68691312+girishfury@users.noreply.github.com>
2024-08-02 17:15:26 +0530

    Update mysql.sh

--- userdata/mysql.sh
+++ userdata/mysql.sh
@@ -8,7 +8,7 @@ sudo dnf install mariadb105-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b main https://github.com/girishfury/vprofile-project.git
+git clone -b aws-LiftAndShift https://github.com/girishfury/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_PASS'"
